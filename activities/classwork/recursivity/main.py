1d42c2e2612cff5debc4927c1c8a82e60387d027
fjbarrg <47005932+fjbarrg@users.noreply.github.com>
2019-12-04 16:36:06 -0600

    Update main.py

--- activities/classwork/recursivity/main.py
+++ activities/classwork/recursivity/main.py
@@ -1,28 +1,28 @@
 import fire
 import logging
+from util import TriangleBuilder, caching, timeit
 
-from util import TriangleBuilder, caching
-
+logger = logging.getLogger(__name__)
 
 class Main(object):
-    builder = TriangleBuilder()
+    builder=TriangleBuilder() #Define a function on a variable
 
-    @staticmethod
-    def message(content):
+    #staticmethod
+    def message(self, content):
         logging.info(content)
 
-    @caching
+    @caching #decorator
     def get_element_recursively(self, i, j):
         return 1 if (j >= i or j == 0) else \
-            self.get_element_recursively(i=i-1, j=j) + \
-            self.get_element_recursively(i=i-1, j=j-1)
+            self.get_element_recursively(i=i - 1, j=j) + \
+            self.get_element_recursively(
+            i=i - 1, j=j - 1)
 
     def pascal_triangle_a(self, level, index=0):
         if index < level:
-            row = [str(self.get_element_recursively(i=index, j=j))
-                   for j in range(index+1)]
+            row = [str(self.get_element_recursively(i=index, j=j)) for j in range(index + 1)]
             print(" ".join(row))
-            self.pascal_triangle_a(level=level, index=index+1)
+            self.pascal_triangle_a(level=level, index=index + 1)
 
     def pascal_triangle_b(self, level, index=0):
         if index < level:
@@ -30,13 +30,14 @@ class Main(object):
             print(" ".join(row))
             self.pascal_triangle_b(level, index=index+1)
 
+    @timeit(logger)
     def pascal_triangle(self, level, option):
         if "a" == option.lower():
             self.pascal_triangle_a(level=level, index=0)
         elif "b" == option.lower():
             self.pascal_triangle_b(level=level, index=0)
         else:
-            logging.info(f"Option {option} undefined.")
+            logger.info(f"Option {option}  undefined.") #.format(option=option))
 
 
 if __name__ == "__main__":
